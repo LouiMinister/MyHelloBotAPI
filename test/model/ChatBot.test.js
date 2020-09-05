9cd80472e10db8d8f17b02f865e7c5291be4cd63
LouiMinister <LouiMinister@gmail.com>
2020-09-06 02:17:28 +0900

    model/ChatBot.js의 유닛테스트 코드 test/model/ChatBot.test.js 구현
    ChatBot.js의 getSkillStartingScript 메서드 테스트 코드 추가

---
+++ test/model/ChatBot.test.js
@@ -0,0 +1,13 @@
+import ChatBot from '../../model/ChatBot'
+
+const chatBot = new ChatBot();
+
+test(" ", async () => {
+    const result = await chatBot.getSkillStartingScript(1);
+    expect(JSON.stringify(result)).toBe(JSON.stringify([
+        {"id": 1, 
+        "chatbot_skill_id" : 1, 
+        "contents": "내꺼인 듯 내꺼 아닌 내꺼 같은 그 분\n과연 사귀게 될 건지\n사귀면 언제 사귈 건지\n::emo::{{ramama-heartsmile}}\n타로로 점 쳐볼까?"
+        , "depth":0}
+    ]));
+});
