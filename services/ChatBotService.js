ee0d4de56ef59e623e1e6fd69285582a76ca0176
LouiMinister <LouiMinister@gmail.com>
2020-09-06 22:11:27 +0900

    ChatBotService 클래스 메서드 추가
    addSkillReview 메서드 추가
    ChatBotServce.test 에 해당 메서드 유닛테스트 로직 추가

--- services/ChatBotService.js
+++ services/ChatBotService.js
@@ -6,20 +6,31 @@ class ChatBotService {
     }
     
     async getFirstScript(skillId){
-        const skillStaringScript = await this.chatBot.getFirstScript(skillId).catch((err)=>{throw err;});
+        const skillStaringScript = await this.chatBot.getFirstScript(skillId)
+            .catch((err)=>{throw err;});
         return skillStaringScript;
     }
 
     async getScript(scriptId){
-        const skillStaringScript = await this.chatBot.getScript(scriptId).catch((err)=>{throw err;});
+        const skillStaringScript = await this.chatBot.getScript(scriptId)
+            .catch((err)=>{throw err;});
         return skillStaringScript;
     }
 
     async getReplyScripts(scriptId){
-        const replyScripts = await this.chatBot.getReplyScripts(scriptId).catch((err)=>{throw err;});
+        const replyScripts = await this.chatBot.getReplyScripts(scriptId)
+            .catch((err)=>{throw err;});
         if (replyScripts.length === 0) {
             return undefined;
         }
+        return replyScripts;
+    }
+
+    async addSkillReview(skillReview){
+        const skillReviewObj = JSON.parse(skillReview);
+        const {user_id, chatbot_skill_id, rating_code, message} = skillReviewObj;
+        const replyScripts = await this.chatBot.addSkillReview(user_id, chatbot_skill_id, rating_code, message)
+            .catch((err)=>{throw err;});
         return replyScripts;
     }
 }
