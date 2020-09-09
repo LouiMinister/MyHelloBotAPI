a02f7b7a84e22a7d94d6dfb2ee9410923444de9d
unknown <dongjae0912@naver.com>
2020-09-09 11:30:06 +0900

    ChatBot클래스의 메서드 리펙토링
    메서드들의 로직 중 쿼리 실행, 커넥션 가져오기 해제하기 로직을 db.js의
    queryExcute 함수로 모듈화

--- models/ChatBot.js
+++ models/ChatBot.js
@@ -1,7 +1,4 @@
-
-import mysql from 'mysql';
-import dbconfig from '../configs/db_config.json';
-import getConnection from '../utils/db';
+import queryExecute from '../utils/db';
 
 class ChatBot {
     constructor () {
@@ -9,77 +6,28 @@ class ChatBot {
     }
     
     async getFirstScript(skillId){
-        const connection = await getConnection()
-            .catch((err) => {
-                throw err;
-            });
         const rawQuery = `SELECT * FROM chatbot_scripts WHERE chatbot_skill_id = ${skillId} AND depth = 0 LIMIT 1`;
-        const promise = new Promise( (resolve, reject) => {
-            connection.query( rawQuery, (err, rows, field) => {
-                if (err) { reject(err); }
-                resolve(rows[0]);
-            });
-        }).catch( (err) => {
-            throw err;
-        }).then((result)=>{
-            connection.release();
-            return result;
-        });
-        return promise; 
+        const rows = await queryExecute(rawQuery);
+        return rows[0]; 
     }
 
     async getScript(scriptId){
-        const connection = await getConnection()
-            .catch((err) => {
-                throw err;
-            });
         const rawQuery = `SELECT * FROM chatbot_scripts WHERE id = ${scriptId}`;
-        const promise = new Promise( (resolve, reject) => {
-            connection.query( rawQuery, (err, rows, field) => {
-                if (err) { reject(err); }
-                resolve(rows[0]);
-            });
-        }).then((result)=>{
-            connection.release();
-            return result;
-        });
-        return promise;
+        const rows = await queryExecute(rawQuery);
+        return rows[0]; 
     }
 
     async getReplyScripts(scriptId){
-        const connection = await getConnection()
-            .catch((err) => {
-                throw err;
-            });
         const rawQuery = `SELECT * FROM chatbot_reply_scripts WHERE prev_chatbot_script_id = ${scriptId}`;
-        const promise = new Promise( (resolve, reject) => {
-            connection.query( rawQuery, (err, rows, field) => {
-                if (err) { reject(err); }
-                resolve(rows);
-            });
-        }).then((result)=>{
-            connection.release();
-            return result;
-        });
-        return promise;
+        const rows = await queryExecute(rawQuery);
+        return rows; 
     }
 
     async addSkillReview(user_id, chatbot_skill_id, rating_id, message){
-        const connection = await getConnection()
-            .catch((err) => {
-                throw err;
-            });
         const rawQuery = `INSERT INTO skill_reviews (user_id, chatbot_skill_id, rating_code, message) VALUES (${user_id}, ${chatbot_skill_id}, ${rating_id}, "${message}")`;
-        const promise = new Promise( (resolve, reject) => {
-            connection.query( rawQuery, (err, rows, field) => {
-                if (err) { reject(err); }
-                resolve();
-            });
-        }).then((result)=>{
-            connection.release();
-            return result;
-        });
-        return promise;
+        const result = await queryExecute(rawQuery);
+        return result;
+        
     }
 }
 
